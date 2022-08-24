01302a104a52414a62dc63dd8cfb6cec5af8b503
prabhudevv <prabhudev.vatnal55@gmail.com>
2022-08-24 20:40:08 +0530

    Added dynamic room ids

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-$('#room-id').val(Math.floor(100000 + Math.random() * 999999));
+let baseUrl = window.location.href;
+let originUrl = window.location.origin + "/";
+
+if (baseUrl !== originUrl) {
+  let roomId = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
+  $('#room-id').val(roomId);
+} else {
+  $('#room-id').val(Math.floor(100000 + Math.random() * 900000));
+}
